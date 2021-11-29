d2337bc695ff96ad2d38b110c769e5637cd863c1
javiervasquez212 <60985490+javiervasquez212@users.noreply.github.com>
2021-11-29 15:00:49 -0600

    Avance/Json/CrearNodos/EnlazarNodos

--- readJson.h
+++ readJson.h
@@ -5,20 +5,38 @@
 #include "fstream"
 #include "iostream"
 #include <vector>
-
 #include <cstdlib>
+#include "atoms.h"
+#include "grafo.h"
+
 
 using json = nlohmann::json;
 
 using namespace std;
 
-void getFromJSON(){
+
+Grafo universo = Grafo(true);
+
+Atom arrayAtomosCreados[3][6];
+
+void crearATomos(){
 
     fstream archivo("universo.json");
-
     json infoUniverso;
-
     archivo>>infoUniverso;
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int x = 0; x < 6; x++)
+        {
+            cout << infoUniverso["Universo"][i][x] << endl;
+            Atom atomoActual = Atom("1");
+            universo.addNode(&atomoActual);
+            arrayAtomosCreados[i][x] = atomoActual;
+        }
+    }
+}
+void enlazarAtomos(){
 
     int cantidadConexiones;
     int tipoElementoAConectar;
@@ -34,9 +52,10 @@ void getFromJSON(){
             while(contadorConexiones != cantidadConexiones)
             {
                 tipoElementoAConectar = rand() %3;
-                if(infoUniverso["Universo"][i][x]!=infoUniverso["Universo"][tipoElementoAConectar][contadorIndiceConexion]){
-                    cout << infoUniverso["Universo"][i][x] << " : " << infoUniverso["Universo"][tipoElementoAConectar][contadorIndiceConexion] << endl;
-
+                if(arrayAtomosCreados[i][x].getName()!=arrayAtomosCreados[tipoElementoAConectar][contadorIndiceConexion].getName()){
+                    int numeroRandom = rand() % 20 +1;
+                    universo.addArc(&arrayAtomosCreados[i][x],&arrayAtomosCreados[tipoElementoAConectar][contadorIndiceConexion],numeroRandom);
+                    //cout << infoUniverso["Universo"][i][x] << " : " << infoUniverso["Universo"][tipoElementoAConectar][contadorIndiceConexion] << endl;
                 }
                 contadorConexiones++;
                 contadorIndiceConexion++;
@@ -46,4 +65,5 @@ void getFromJSON(){
         }
     }
 }
+
 #endif 
